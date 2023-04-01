c2b0902fc3394b789ccd3542af7d21d0f9476ff9
Hernandez David <acevedodavid15@gmail.com>
2023-04-01 20:35:26 -0300

    Actualizado

--- Tercera_Entrega/models.py
+++ Tercera_Entrega/models.py
@@ -6,11 +6,20 @@ class Empleados(models.Model):
     Cargo_Empleado = models.CharField(max_length=50)
     Fecha_Ingreso = models.DateField(null=False)
 
+    def __str__(self):
+        return f"{self.id} - {self.Nombre_Empleado} - {self.Apellido_Empleado}"
+
 
 class AreaEmpresa(models.Model):
     Nombre_Area = models.CharField(max_length=30)
     Cantidad_Trabajadores = models.PositiveIntegerField(default=False)
 
+    def __str__(self):
+        return f"{self.id} - {self.Nombre_Area} - Cantidad de Trabajadores: {self.Cantidad_Trabajadores}"
+
 class Sucursal(models.Model):
     Ciudad = models.CharField(max_length=30)
     Numero_Sucursal = models.PositiveIntegerField(default=False)
+
+    def __str__(self):
+        return f"{self.id} - {self.Ciudad} - Numero de Sucursal: {self.Numero_Sucursal}"
--- Tercera_Entrega/views.py
+++ Tercera_Entrega/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 from django.views.generic import ListView, CreateView
+from Tercera_Entrega.models import Empleados, AreaEmpresa, Sucursal
+from django.urls import reverse_lazy
+
+def index(request):
+    return render(request,"Tercera_Entrega/index.html")
 
 class EmpleadosList(ListView):
     model = Empleados
@@ -12,12 +17,25 @@ class SucursalList(ListView):
 
 class EmpleadosCreate(CreateView):
     model = Empleados
+    success_url = reverse_lazy("empleados-create")
     fields = '__all__'
 
 class AreaEmpresaCreate(CreateView):
     model = AreaEmpresa
+    success_url = reverse_lazy("areaempresa-create")
     fields = '__all__'
 
 class SucursalCreate(CreateView):
-    model = Empleados
+    model = Sucursal
+    success_url = reverse_lazy("sucursal-create")
     fields = '__all__'
+
+class BuscarEmpleados(ListView):
+    model = Empleados
+    context_object_name = 'empleados'
+
+    def get_query(self):
+        f = BuscarEmpleadosForm(self.request.GET)
+        if f.is_valid():
+            return empleados.objects.filter(nombre__icontains=f.data["criterio_Nombre"]).all()
+        return Empleados.objects.none()
